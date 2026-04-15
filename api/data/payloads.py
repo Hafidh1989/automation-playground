cd379c4ab3ccedb2f0c45b41518c62f4c9928c4d
Hafidh1989 <ahmadhafiidhkhoiri@gmail.com>
2026-04-15 12:59:47 +0700

    add api

---
+++ api/data/payloads.py
@@ -0,0 +1,11 @@
+VALID_LOGIN_PAYLOAD = {
+    "username": "emilys",
+    "password": "emilyspass",
+    "expiresInMins": 30,
+}
+
+INVALID_LOGIN_PAYLOAD = {
+    "username": "emilys",
+    "password": "wrong_password",
+    "expiresInMins": 30,
+}
